d9ca96c10d8c8a73cfaf4f3c66587d50c3749f3a
lucas <lucas.lc16@aluno.ifsc.edu.br>
2025-03-31 22:05:39 -0300

    Lab 02 - Exercício 3 atualizado.

--- lab-02/Exercicio03.java
+++ lab-02/Exercicio03.java
@@ -7,38 +7,33 @@ public class Exercicio03{
         System.out.println("Entre com a idade do morador (digite um número negativo para encerrar): ");
         int idade = leitor.nextInt();
 
-        int qtd0a18 = 0;
-        int qtd19a35 = 0;
-        int qtd36a50 = 0;
-        int qtd51a65 = 0;
-        int qtd66a80 = 0;
-        int qtd81a100 = 0;
+        int[] qtd = new int[6];
 
         while(idade >= 0){
             if (idade <=18){
-                qtd0a18++;
+                qtd[0]++;
             } else if (idade <= 35) {
-                qtd19a35++;
+                qtd[1]++;
             } else if (idade <= 50) { 
-                qtd36a50++;
+                qtd[2]++;
             } else if (idade <= 65) {
-                qtd51a65++;
+                qtd[3]++;
             } else if (idade <= 80) {
-                qtd66a80++;
+                qtd[4]++;
             } else if (idade <= 100) {
-                qtd81a100++;
+                qtd[5]++;
             }
             System.out.println("Entre com a idade do morador (digite um número negativo para encerrar): ");  
             idade = leitor.nextInt();         
         }
 
         System.out.println("=======================");
-        System.out.println("0 a 18: " + "*".repeat(qtd0a18));
-        System.out.println("19 a 35: " + "*".repeat(qtd19a35));
-        System.out.println("36 a 50: " + "*".repeat(qtd36a50));
-        System.out.println("51 a 65: " + "*".repeat(qtd51a65));
-        System.out.println("66 a 80: " + "*".repeat(qtd66a80));
-        System.out.println("80 a 100: " + "*".repeat(qtd81a100));
+        System.out.println("0 a 18: " + "*".repeat(qtd[0]));
+        System.out.println("19 a 35: " + "*".repeat(qtd[1]));
+        System.out.println("36 a 50: " + "*".repeat(qtd[2]));
+        System.out.println("51 a 65: " + "*".repeat(qtd[3]));
+        System.out.println("66 a 80: " + "*".repeat(qtd[4]));
+        System.out.println("80 a 100: " + "*".repeat(qtd[5]));
         System.out.println("=======================");
     }
 }
