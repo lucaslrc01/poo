6e26ff0fbeb3e46aa6931506313d0c14ab7591d8
lucas <lucas.lc16@aluno.ifsc.edu.br>
2025-03-31 22:05:25 -0300

    Lab 02 - Exercício 1 atualizado.

--- lab-02/Exercicio01.java
+++ lab-02/Exercicio01.java
@@ -8,37 +8,20 @@ public class Exercicio01{
         String mes = leitor.nextLine();
 
         System.out.println("Dia:");
-        int dia = leitor.nextInt();
-
-        String estacao = "";
+        int dia = leitor.nextInt().toLowerCase();
 
         // Considerando as informações obtidas em https://www.calendarr.com/brasil/estacoes-do-ano/
-        switch (mes) {
-            case "janeiro", "fevereiro":
-                estacao = "Verão";
-                break;
-            case "março":
-                estacao = (dia > 19) ? "Outono" : "Verão"; // Outono: 20/03 -> 20/06
-                break;
-            case "abril", "maio":
-                estacao = "Outono";
-            case "junho":
-                estacao = (dia > 20) ? "Inverno" : "Outono"; // Inverno: 21/06 -> 21/09
-                break;
-            case "julho", "agosto":
-                estacao = "Inverno";
-                break;
-            case "setembro": 
-                estacao = (dia > 21) ? "Primavera" : "Inverno"; // Primavera: 22/09 -> 20/12
-                break;
-            case "outubro", "novembro":
-                estacao = "Primavera";
-                break;
-            case "dezembro":
-                estacao = (dia > 20) ? "Verão" : "Primavera";  // Verão: 21/12 -> 19/03
-            default:
-                break;
-        }
+        String estacao = switch (mes) {
+            case "janeiro", "fevereiro" -> "Verão";
+            case "março" -> (dia > 19) ? "Outono" : "Verão"; // Outono: 20/03 -> 20/06
+            case "abril", "maio" -> "Outono";
+            case "junho" -> (dia > 20) ? "Inverno" : "Outono"; // Inverno: 21/06 -> 21/09
+            case "julho", "agosto" -> "Inverno";
+            case "setembro" -> (dia > 21) ? "Primavera" : "Inverno"; // Primavera: 22/09 -> 20/12
+            case "outubro", "novembro" -> "Primavera";
+            case "dezembro" -> (dia > 20) ? "Verão" : "Primavera";  // Verão: 21/12 -> 19/03
+            default -> "Inexistente";
+        };
 
         System.out.println("O dia " + dia + " de " + mes + " cai na seguinte estação: " + estacao);
     }
